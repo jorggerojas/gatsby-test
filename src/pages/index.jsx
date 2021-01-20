fd6aca5adc25e2c6e78e6a446340ba8e1bef29dc
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2021-01-19 18:27:49 -0600

    init: create a project

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Helmet } from 'react-helmet'
+export default () => {
+    return (
+        <>
+            {/* <Helmet>
+
+            </Helmet> */}
+            <h1>Hello world!</h1>
+        </>
+    );
+};
