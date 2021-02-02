43065d2d5e2c2bb0f77b6f5d56e8fb6fcf0388d3
Juan Olvera <jp.olvera@viaducto.io>
2021-02-02 18:36:24 +0000

    feat: use Spacer and Hideable instead of padding

--- src/organisms/MiniPost/MiniPost.jsx
+++ src/organisms/MiniPost/MiniPost.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled, { css } from 'styled-components';
 import StyledMiniPost from './StyledMiniPost';
 import { Title } from '../../cells/Title';
 import { Paragraph } from '../../cells/Paragraph';
 import { BackgroundImage } from '../../cells/BackgroundImage';
-import { PostDescription } from '../../tissues/PostDescription';
+import PostLabels from '../../organs/PostLabels';
 import Author from '../../organs/Author';
-import padding from '../../utils/padding';
 import config from '../../utils/config';
+import Spacer from '../../cells/Spacer';
+import Hideable from '../../cells/Hideable';
 
 const { display, breakpoints } = config;
 const SideImage = styled.img`
@@ -61,43 +62,40 @@ const PostRow = styled(Row)`
   flex-direction: column;
   padding-left: 2.375rem;
   padding-right: 0.938rem;
-  padding-top: ${({ cover }) => (cover === 'true' ? display.mobile.sm : '0')};
-  padding-bottom: ${({ cover }) =>
-    cover === 'true' ? display.mobile.sm : '0'};
+  padding-top: ${({ cover }) => (cover === true ? display.mobile.sm : '0')};
+  padding-bottom: ${({ cover }) => (cover === true ? display.mobile.sm : '0')};
   justify-content: center;
   @media (min-width: ${breakpoints.sm}) {
     padding-left: ${display.mobile.sm};
     padding-right: ${display.mobile.sm};
-    padding-top: ${({ cover }) => (cover === 'true' ? display.mobile.sm : '0')};
+    padding-top: ${({ cover }) => (cover === true ? display.mobile.sm : '0')};
     padding-bottom: ${({ cover }) =>
-      cover === 'true' ? display.mobile.sm : '0'};
+      cover === true ? display.mobile.sm : '0'};
   }
   @media (min-width: ${breakpoints.md}) {
     flex-direction: column;
     align-items: center;
     padding-left: ${display.mobile.sm};
     padding-right: ${display.mobile.sm};
-    padding-top: ${({ cover }) => (cover === 'true' ? display.mobile.sm : '0')};
+    padding-top: ${({ cover }) => (cover === true ? display.mobile.sm : '0')};
     padding-bottom: ${({ cover }) =>
-      cover === 'true' ? display.mobile.sm : '0'};
+      cover === true ? display.mobile.sm : '0'};
   }
   @media (min-width: ${breakpoints.lg}) {
     flex-direction: row;
     padding-left: 2.063rem;
     padding-right: 6.951rem;
-    padding-top: ${({ cover }) =>
-      cover === 'true' ? display.desktop.sm : '0'};
+    padding-top: ${({ cover }) => (cover === true ? display.desktop.sm : '0')};
     padding-bottom: ${({ cover }) =>
-      cover === 'true' ? display.desktop.sm : '0'};
+      cover === true ? display.desktop.sm : '0'};
   }
   @media (min-width: ${breakpoints.xl}) {
     flex-direction: row;
     padding-left: 10.313rem;
     padding-right: 10.389rem;
-    padding-top: ${({ cover }) =>
-      cover === 'true' ? display.desktop.sm : '0'};
+    padding-top: ${({ cover }) => (cover === true ? display.desktop.sm : '0')};
     padding-bottom: ${({ cover }) =>
-      cover === 'true' ? display.desktop.sm : '0'};
+      cover === true ? display.desktop.sm : '0'};
   }
 `;
 const PostCol = styled(Col)`
@@ -106,6 +104,7 @@ const PostCol = styled(Col)`
   align-items: flex-start;
   justify-content: start;
   max-width: 585px;
+  padding: 0;
   @media (min-width: ${breakpoints.sm}) {
     display: flex;
     align-items: flex-start;
@@ -123,48 +122,43 @@ const PostCol = styled(Col)`
 `;
 const MiniPost = ({ cover, type, info, title, text, src, alt }) => {
   return (
-    <StyledMiniPost fluid cover={cover.toString()}>
+    <StyledMiniPost fluid cover={cover}>
       {cover ? <BackgroundImage src={src} alt={alt} /> : null}
-      <PostRow cover={cover.toString()}>
+      <PostRow cover={cover}>
         {type !== 'blog' && !cover ? (
-          <PostCol css={padding({ right: 'xl', bottom: 'xl' })} xs>
+          <PostCol>
             <SideImage src={src} alt={alt} type={type} />
           </PostCol>
         ) : null}
-
+        <Hideable visibleOn="lg">
+          <Spacer size="xxl" direction="horizontal" />
+        </Hideable>
         <Col
-          css={padding({ vertical: 'sm', horizontal: 0 })}
           style={{
-            maxWidth: type === 'blog' ? '538px' : '585px',
+            maxWidth: '585px',
+            padding: 0,
           }}
         >
-          <Container fluid>
-            <Row>
-              <Col md={10} css={padding({ size: 0 })}>
-                <PostDescription
-                  info={info}
-                  type={type}
-                  align="start"
-                  justify="start"
-                />
-              </Col>
-            </Row>
-          </Container>
-          <Title
-            level="4"
-            weight="400"
-            lineHeight="2.243rem"
-            css={padding({ bottom: 'md' })}
-          >
+          <Row>
+            <Col md={10} style={{ padding: 0 }}>
+              <PostLabels
+                reading_time={7}
+                type={type}
+                category="SALES AND MARKETING, ENGINEERING"
+                align="start"
+                justify="start"
+              />
+            </Col>
+          </Row>
+          <Title level="4" weight="400" lineHeight="2.243rem">
             {title}
           </Title>
-          <Paragraph
-            lineHeight="1.5"
-            weight="300"
-            css={padding({ bottom: 'xl' })}
-          >
+          <Spacer size="md" />
+          <Paragraph lineHeight="1.5" weight="300">
             {text}
           </Paragraph>
+          <Spacer size="xl" />
+
           {type === 'free-download' ? null : (
             <Author
               name="Clara González"
--- src/organisms/MiniPost/StyledMiniPost.js
+++ src/organisms/MiniPost/StyledMiniPost.js
@@ -1,25 +1,18 @@
 import styled from 'styled-components';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import config from '../../utils/config';
+import padding from '../../utils/padding';
 
-const { display, breakpoints } = config;
 const ContainerMiniPost = styled(Container)`
-  padding: 0 !important;
+  padding: 0;
 `;
 const StyledMiniPost = styled(ContainerMiniPost)`
-  display: ${({ cover }) => cover === 'true' ? '' : 'flex'};
+  display: ${({ cover }) => (cover === true ? '' : 'flex')};
   justify-content: center;
   margin: 0;
   width: 100%;
-  padding: ${({ cover }) => cover === 'true' ? '0' : `${display.mobile.sm} 0 !important`};
   transition: padding 0.25s cubic-bezier(0.12, 0, 0.39, 0);
-  @media (min-width: ${breakpoints.lg}) {
-    padding: ${({ cover }) => cover === 'true' ? '0' : `${display.desktop.sm} 0 !important`};
-  }
-  @media (min-width: ${breakpoints.xl}) {
-    padding: ${({ cover }) => cover === 'true' ? '0' : `${display.desktop.sm} 0 !important`};
-  }
+  ${padding({ top: 'sm' })};
 `;
 
 export default StyledMiniPost;
