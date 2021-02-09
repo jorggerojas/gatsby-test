99d00165b491ef31f72bffa0c431754dda7673eb
Juan Olvera <jp.olvera@viaducto.io>
2021-02-09 00:45:38 +0000

    feat: use useQuery to load posts dynamically

--- src/pages/blog-roll.js
+++ src/pages/blog-roll.js
@@ -1,69 +1,121 @@
 import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { css } from 'styled-components';
 import GlobalStyle from '../utils/global';
 import MiniPost from '../organisms/MiniPost';
 import Newsletter from '../organisms/Newsletter';
-import Data from '../hooks/postsData';
 
-const Blog = () => {
-  const dataPosts = Data();
-  const [page, setPage] = useState(10);
-  const [postData, setPostData] = useState([]);
-  const [hasMoreContent, setHasMoreContent] = useState(true);
-  const loadData = () => {
-    if ((postData.length >= dataPosts.length)) {
-      setHasMoreContent(false);
-      return;
+const GET_POSTS = gql`
+  query GetPosts($skip: Int!, $limit: Int!) {
+    allGhostPost(
+      sort: { fields: [created_at], order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          id
+          title
+          feature_image
+          excerpt
+          slug
+          authors {
+            name
+            profile_image
+          }
+        }
+      }
+      totalCount
     }
-    setTimeout(() => {
-      setPostData(postData.concat(dataPosts.slice(page, (page + 10))));
-      setPage((page + 10));
-    }, 400);
   }
+`;
+const Blog = () => {
+  let limit = 2;
+  const [skip, setSkip] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const { loading, error, data, refetch } = useQuery(GET_POSTS, {
+    variables: { skip, limit },
+  });
+
   useEffect(() => {
-    setPostData(postData.concat(dataPosts.slice(0, page)));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (data && data.allGhostPost.edges.length > 0) {
+      setTotalCount(data.allGhostPost.totalCount);
+      setPosts((previousPosts) => [
+        ...previousPosts,
+        ...data.allGhostPost.edges,
+      ]);
+      setSkip((s) => s + data.allGhostPost.edges.length);
+    }
+  }, [data]);
+
+  const [hasMore, setHasMore] = useState(true);
+
+  const loadMorePosts = () => {
+    if (posts.length < totalCount) {
+      setHasMore(true);
+      setTimeout(() => {
+        refetch({
+          limit,
+          skip: posts.length,
+        });
+      }, 5000);
+    } else {
+      setHasMore(false);
+    }
+  };
+
   return (
     <>
       <GlobalStyle />
       <InfiniteScroll
-        css={css`overflow: hidden !important;`}
+        css={css`
+          overflow: hidden !important;
+        `}
         data-sal="slide-up"
         data-sal-delay="1"
         data-sal-easing="ease"
-        dataLength={postData.length}
-        next={loadData}
-        loader={<h4>Loading...</h4>}
-        hasMore={hasMoreContent}
+        dataLength={totalCount}
+        next={loadMorePosts}
+        loader={<h4>Cargando...</h4>}
+        hasMore={hasMore}
         endMessage={
           <p style={{ textAlign: 'center' }}>
             <b>Yay! You have seen it all</b>
           </p>
         }
       >
-        {postData.map(({ node }, index) => {
-          return (
-            <div key={`n${index}`}>
-              {(index % 10 === 0 && index !== 0) || (index === dataPosts.length) ? <Newsletter /> : null}
-              <MiniPost
-                cover={index.toString().charAt(index.toString().length - 1) === "0"}
-                key={index}
-                data-sal="fade"
-                data-sal-delay="100"
-                data-sal-easing="easeIn"
-                type={'video'}
-                info={'jijiji'}
-                title={node.title}
-                text={node.excerpt}
-                src={node.feature_image}
-                alt={node.feature_image}
-                author={node.authors[0]}
-              />
-            </div >
-          )
-        })}
+        {posts.length > 0 &&
+          posts.map(({ node }, index) => {
+            return (
+              <div key={`post-${index}-${node.title}`}>
+                {(index % 10 === 0 && index !== 0) || index === totalCount ? (
+                  <Newsletter />
+                ) : null}
+                <MiniPost
+                  cover={
+                    index.toString().charAt(index.toString().length - 1) === '0'
+                  }
+                  key={index}
+                  data-sal="fade"
+                  data-sal-delay="100"
+                  data-sal-easing="easeIn"
+                  type={'video'}
+                  info={'jijiji'}
+                  title={node.title}
+                  text={node.excerpt}
+                  src={node.feature_image}
+                  alt={`Imagen de ${node.title}`}
+                  author={node.authors[0]}
+                />
+              </div>
+            );
+          })}
+        {loading && posts.length == 0 ? 'Cargando...' : null}
+        {error &&
+          'Ocurrió un error con el servidor, y no hemos podido consultar ningún post'}
       </InfiniteScroll>
     </>
   );
