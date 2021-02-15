b1cb965fe3f27a90828e46a985e0b41be9d95810
Juan Olvera <jp.olvera@viaducto.io>
2021-02-15 17:22:38 +0000

    test(a11y): test Button

--- src/cells/Button/__tests__/Button.test.js
+++ src/cells/Button/__tests__/Button.test.js
@@ -6,6 +6,14 @@ describe('<Button/>', () => {
   test('should render properly', () => {
     render(<Button label="Continue" />);
   });
+
+  test('should have focus', () => {
+    render(<Button label="Accept" />);
+    const button = screen.getByRole('button');
+    button.focus();
+    expect(button).toHaveFocus();
+  });
+
   test('should call onClick function once', () => {
     const handleClick = jest.fn();
     render(<Button label="Continue" onClick={handleClick} />);
@@ -16,7 +24,10 @@ describe('<Button/>', () => {
   test('should not call onClick function', () => {
     const handleClick = jest.fn();
     render(<Button label="Continue" onClick={handleClick} disabled={true} />);
-    fireEvent.click(screen.getByText('Continue'));
+    const button = screen.getByRole('button');
+    button.focus();
+    expect(button).not.toHaveFocus();
+    fireEvent.click(button);
     expect(handleClick).not.toHaveBeenCalled();
   });
 });
